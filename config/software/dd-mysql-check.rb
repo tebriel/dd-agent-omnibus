5fb4c81b48cb1e985b4b99693e826af16e52aadc
Tristan Michelet <tristan@datadoghq.com>
2015-12-02 16:47:17 -0500

    [POC] more debug

--- config/software/dd-mysql-check.rb
+++ config/software/dd-mysql-check.rb
@@ -3,5 +3,6 @@ default_version "0.0.1"
 
 build do
   puts 'hello world'
+  command "ls #{install_dir}/embedded/bin/"
   # command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" boto3"
 end
