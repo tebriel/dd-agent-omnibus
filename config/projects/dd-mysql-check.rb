f44569c680bf5b888f21ddb5140315cb20128e2f
Tristan Michelet <tristan@datadoghq.com>
2015-12-02 14:41:20 -0500

    [POC] Add fake mysql-check project

---
+++ config/projects/dd-mysql-check.rb
@@ -0,0 +1,24 @@
+require "./lib/ostools.rb"
+
+name 'dd-mysql-check'
+maintainer 'Datadog Packages <package@datadoghq.com>'
+homepage 'http://www.datadoghq.com'
+install_dir '/opt/datadog-agent'
+
+
+# ------------------------------------
+# Dependencies
+# ------------------------------------
+
+# creates required build directories
+dependency 'preparation'
+
+# Datadog agent
+dependency 'datadog-agent'
+dependency 'dd-mysql-check'
+
+# version manifest file
+dependency 'version-manifest'
+
+exclude '\.git*'
+exclude 'bundler\/git'
---
+++ config/software/dd-mysql-check.rb
@@ -0,0 +1,6 @@
+name "dd-integrations-mysql"
+default_version "0.0.1"
+
+build do
+
+end
