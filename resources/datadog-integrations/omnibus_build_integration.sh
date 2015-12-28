1b5ea477cb3c48ac44bb0bc8c7d921a33ab46b16
Tristan Michelet <tristan@datadoghq.com>
2015-12-28 16:01:10 +0100

    Use ERB files

--- resources/datadog-integrations/omnibus_build_integration.sh
+++ resources/datadog-integrations/omnibus_build_integration.sh
@@ -2,8 +2,11 @@ NAME=$1
 echo "building integration $NAME"
 
 echo "generating files"
-cp resources/datadog-integrations/project.rb config/projects/dd-$NAME-check.rb
-cp resources/datadog-integrations/software.rb config/software/dd-$NAME.rb
+(echo '<% name="$1" %>' && resources/datadog-integrations/project.rb) | erb > config/projects/dd-$NAME-check.rb
+(echo '<% name="$1" %>' && resources/datadog-integrations/software.rb) | erb > config/software/dd-$NAME.rb
 
 echo "building project"
 bin/omnibus build dd-$NAME-check
+
+
+(echo '<% name="Joe"; quality="fantastic" %>' && cat 1.erb) | erb
